c58618839a9d9b88d4ca1930d29bbe3d6371c7cf
Elad Bernard Haviv <elad.haviv@gmail.com>
2024-05-23 15:05:02 +0300

    .

---
+++ client/src/components/Products/Product.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export type ProductProps = {
+    id?: string;
+};
+
+const Product = (props: ProductProps) => {
+    return <div>Product</div>;
+};
+
+export default Product;
